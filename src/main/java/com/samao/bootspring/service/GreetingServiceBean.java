9bf793955322ed12b55d5856d6a45ac262529040
samao <hsamao86@gmail.com>
2015-12-28 22:58:32 -0500

    Added Unit Test

--- src/main/java/com/samao/bootspring/service/GreetingServiceBean.java
+++ src/main/java/com/samao/bootspring/service/GreetingServiceBean.java
@@ -2,6 +2,8 @@ package com.samao.bootspring.service;
 
 import com.samao.bootspring.model.Greeting;
 import com.samao.bootspring.repository.GreetingRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
@@ -10,6 +12,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityExistsException;
+import javax.persistence.NoResultException;
 import java.util.Collection;
 
 /**
@@ -22,68 +26,113 @@ import java.util.Collection;
         readOnly = true)
 public class GreetingServiceBean implements GreetingService {
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    /**
+     * The Spring Data repository for Greeting entities.
+     */
     @Autowired
     private GreetingRepository greetingRepository;
 
     @Override
     public Collection<Greeting> findAll() {
+        logger.info("> findAll");
+
         Collection<Greeting> greetings = greetingRepository.findAll();
+
+        logger.info("< findAll");
         return greetings;
     }
 
     @Override
-    @Cacheable(value = "greetings",
+    @Cacheable(
+            value = "greetings",
             key = "#id")
     public Greeting findOne(Long id) {
+        logger.info("> findOne id:{}", id);
+
         Greeting greeting = greetingRepository.findOne(id);
+
+        logger.info("< findOne id:{}", id);
         return greeting;
     }
 
     @Override
-    @Transactional(propagation = Propagation.REQUIRED,
+    @Transactional(
+            propagation = Propagation.REQUIRED,
             readOnly = false)
-    @CachePut(value = "greetings", key = "#result.id")
+    @CachePut(
+            value = "greetings",
+            key = "#result.id")
     public Greeting create(Greeting greeting) {
+        logger.info("> create");
+
+        // Ensure the entity object to be created does NOT exist in the
+        // repository. Prevent the default behavior of save() which will update
+        // an existing entity if the entity matching the supplied id exists.
         if (greeting.getId() != null) {
             // Cannot create Greeting with specified ID value
-            return null;
+            logger.error(
+                    "Attempted to create a Greeting, but id attribute was not null.");
+            throw new EntityExistsException(
+                    "The id attribute must be null to persist a new entity.");
         }
+
         Greeting savedGreeting = greetingRepository.save(greeting);
 
-        // Illustrate TX rollback
-        if (savedGreeting.getId() == 4L){
-            throw new RuntimeException("Roll me back!");
-        }
+        logger.info("< create");
         return savedGreeting;
     }
 
     @Override
-    @Transactional(propagation = Propagation.REQUIRED,
+    @Transactional(
+            propagation = Propagation.REQUIRED,
             readOnly = false)
-    @CachePut(value = "greetings", key = "#greeting.id")
+    @CachePut(
+            value = "greetings",
+            key = "#greeting.id")
     public Greeting update(Greeting greeting) {
-        Greeting greetingPersisted = findOne(greeting.getId());
-        if (greetingPersisted == null) {
+        logger.info("> update id:{}", greeting.getId());
+
+        // Ensure the entity object to be updated exists in the repository to
+        // prevent the default behavior of save() which will persist a new
+        // entity if the entity matching the id does not exist
+        Greeting greetingToUpdate = findOne(greeting.getId());
+        if (greetingToUpdate == null) {
             // Cannot update Greeting that hasn't been persisted
-            return null;
+            logger.error(
+                    "Attempted to update a Greeting, but the entity does not exist.");
+            throw new NoResultException("Requested entity not found.");
         }
-        Greeting updatedGreeting = greetingRepository.save(greeting);
+
+        greetingToUpdate.setText(greeting.getText());
+        Greeting updatedGreeting = greetingRepository.save(greetingToUpdate);
+
+        logger.info("< update id:{}", greeting.getId());
         return updatedGreeting;
     }
 
     @Override
-    @Transactional(propagation = Propagation.REQUIRED,
+    @Transactional(
+            propagation = Propagation.REQUIRED,
             readOnly = false)
-    @CacheEvict(value = "greetings", key = "#id")
+    @CacheEvict(
+            value = "greetings",
+            key = "#id")
     public void delete(Long id) {
+        logger.info("> delete id:{}", id);
 
         greetingRepository.delete(id);
 
+        logger.info("< delete id:{}", id);
     }
 
     @Override
-    @CacheEvict(value = "greetings", allEntries = true)
+    @CacheEvict(
+            value = "greetings",
+            allEntries = true)
     public void evictCache() {
-
+        logger.info("> evictCache");
+        logger.info("< evictCache");
     }
 }
--- src/test/java/com/samao/bootspring/AbstractTest.java
+++ src/test/java/com/samao/bootspring/AbstractTest.java
@@ -1,18 +1,21 @@
 package com.samao.bootspring;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+/**
+ * Created by samao on 12/28/15.
+ */
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 @WebAppConfiguration
-public class ApplicationTests {
+public abstract class AbstractTest {
 
-	@Test
-	public void contextLoads() {
-	}
-
+    protected Logger logger = LoggerFactory.getLogger(this.getClass());
 }
